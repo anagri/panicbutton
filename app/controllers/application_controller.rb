ab472fbf53d1f4d2027b13f370d84868eb4228ed
Amiruddin Nagri <amir.nagri+git@gmail.com>
2012-02-18 18:38:04 +0000

    amir - fixed bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :start_session
+
+  def start_session
+    if session[:session_id].blank?
+      session[:session_id] = (0...8).map{65.+(rand(25)).chr}.join
+    end
+  end
 end
