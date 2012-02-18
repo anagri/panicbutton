65b9c53c645eed3c3ed69480da87a1e588a09ef2
Amiruddin Nagri <amir.nagri+git@gmail.com>
2012-02-18 11:59:33 +0000

    amir/yu - rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panicbutton::Application.config.secret_token = '2528651788e3d093673bdd38acfa7db0bd6cd42fc9e4bcee835270de542080aa18d264f69e7804f227748899d7f56bc4632852824aa69e25ab6ab66851783409'
